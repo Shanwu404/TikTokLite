1892845e8b2ef1db257ae58eb7dd5cf5dfc98ad5
Mary <2567984254@qq.com>
2023-08-05 18:33:45 +0800

    userDao基础功能测试完成

---
+++ dao/userDao_test.go
@@ -0,0 +1,36 @@
+package dao
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInsertUser(t *testing.T) {
+	Init()
+	user1 := User{
+		Username: "John",
+		Password: "password",
+	}
+	err := InsertUser(&user1)
+	fmt.Println(err)
+}
+
+func TestQueryUserByID(t *testing.T) {
+	Init()
+	user, err := QueryUserByID(1)
+	fmt.Println(user)
+	fmt.Println(err)
+}
+
+func TestQueryUserByUsername(t *testing.T) {
+	Init()
+	user, err := QueryUserByUsername("John")
+	fmt.Println(user)
+	fmt.Println(err)
+}
+
+func TestQueryAllNames(t *testing.T) {
+	Init()
+	usernames := QueryAllNames()
+	fmt.Println(usernames)
+}
