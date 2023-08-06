712e434674d2814d2c264f8c56e726363b408194
Mary <2567984254@qq.com>
2023-08-06 08:42:48 +0800

    增加 UserService 接口定义

---
+++ service/userService.go
@@ -0,0 +1,17 @@
+package service
+
+import "github.com/Shanwu404/TikTokLite/dao"
+
+type UserService interface {
+	// QueryUserByUsername 根据name获取User对象
+	QueryUserByUsername(username string) (dao.User, error)
+
+	// QueryUserByID 根据id获取User对象
+	QueryUserByID(id int64) (dao.User, error)
+
+	// Register 用户注册
+	Register(username string, password string) (int64, int32, string)
+
+	// Login 用户登录
+	Login(username string, password string) (int32, string)
+}
