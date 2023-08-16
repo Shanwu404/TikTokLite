6669cdb592a02309d50d381db71280d7cf38a6ab
Mary <2567984254@qq.com>
2023-08-16 19:28:53 +0800

    添加 关系模块接口路由

--- router.go
+++ router.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/Shanwu404/TikTokLite/controller"
-	"github.com/Shanwu404/TikTokLite/service"
 	"github.com/Shanwu404/TikTokLite/utils/auth"
 	"github.com/gin-gonic/gin"
 )
@@ -12,10 +11,9 @@ func NewRouter() *gin.Engine {
 	apiRouter := r.Group("/douyin")
 	apiRouter.Static("tiktok", "./videos") //测试用。配置静态资源路径
 
-	userService := service.NewUserService()                     // 实例化 UserService
-	userController := controller.NewUserController(userService) // 实例化 UserController
-
+	userController := controller.NewUserController()
 	videoController := controller.NewVideoController()
+	relationController := controller.NewRelationController()
 	// basic apis
 	apiRouter.GET("/feed/", videoController.Feed)
 	apiRouter.POST("/publish/action/", auth.Auth, videoController.PublishAction)
@@ -24,6 +22,8 @@ func NewRouter() *gin.Engine {
 	apiRouter.POST("/user/register/", userController.Register)
 	apiRouter.POST("/user/login/", userController.Login)
 	apiRouter.GET("/user/", userController.GetUserInfo)
+
+	apiRouter.POST("/relation/action/", auth.Auth, relationController.RelationAction)
 
 	apiRouter.POST("/comment/action/", auth.Auth, controller.CommentAction)
 	apiRouter.GET("/comment/list/", auth.Auth, controller.CommentList)
