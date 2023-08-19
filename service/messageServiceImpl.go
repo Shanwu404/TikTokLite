e94ff3c252e26a9e541b3e21c7813000835bc867
Shanwu <byr_lc@163.com>
2023-08-20 01:07:15 +0800

    问题注释

--- service/messageServiceImpl.go
+++ service/messageServiceImpl.go
@@ -19,6 +19,8 @@ func (MessageServiceImpl) QueryMessagesByIdsAfter(fromUserId int64, toUserId int
 	if err != nil {
 		log.Println("error:", err.Error())
 	}
+	//这样的处理逻辑放在实际生产环境中其实不是好的解决方案，比如发送时对方刚好也发消息，并且对方消息实际插入数据库时间比”我“的早
+	//主要问题还是这块的前端逻辑不好
 	if len(messages) > 0 && msgTime.UnixMilli() != 0 {
 		messages = messages[1:]
 	}
