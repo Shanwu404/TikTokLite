42172b19108434d2b90593a37ed302c604a4cb3a
Mary <2567984254@qq.com>
2023-08-24 18:40:33 +0800

    user模块加入redis优化

--- utils/redisConstants.go
+++ utils/redisConstants.go
@@ -5,3 +5,7 @@ const CommentCommentKey = "comment:comment:"
 
 // CommentVideoKey key:videoId value:commentIds
 const CommentVideoKey = "comment:video:"
+
+// user模块
+const UserIdKey = "user:id:"     // UserIdKey key:userId value:User_struct
+const UserNameKey = "user:name:" // UserNameKey key:username value:userId
