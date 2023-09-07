17d44e16c4874e7a7a545d32b5168fa5abc3fbcd
qianlinyi <327120052@qq.com>
2023-09-07 23:57:00 +0800

    comment模块添加参数校验

--- controller/commentController.go
+++ controller/commentController.go
@@ -2,12 +2,12 @@ package controller
 
 import (
 	"net/http"
-	"strconv"
 	"sync"
 	"time"
 
 	"github.com/Shanwu404/TikTokLite/service"
 	"github.com/Shanwu404/TikTokLite/utils"
+	"github.com/Shanwu404/TikTokLite/utils/validation"
 	"github.com/gin-gonic/gin"
 )
 
@@ -41,20 +41,21 @@ func NewCommentController() *CommentController {
 
 // CommentAction POST /douyin/comment/action/ 评论操作
 func (cc *CommentController) CommentAction(c *gin.Context) {
-	actionType := c.Query("action_type")
-	if actionType == "1" {
-		content := c.Query("comment_text")
-		content = utils.Filter.Replace(content, '#')
-		// 获取当前用户
-		userId := c.GetInt64("id")
+	req, valid := validation.CommentActionParseAndValidateParams(c)
+	if !valid {
+		c.JSON(http.StatusBadRequest, douyinPublishActionResponse{
+			Response: Response{-1, "Invalid Request."},
+		})
+		return
+	}
+	if req.ActionType == "1" {
+		content := utils.Filter.Replace(req.Content, '#')
 		// 获取当前视频
-		id := c.Query("video_id")
-		videoId, _ := strconv.ParseInt(id, 10, 64)
-		video := cc.videoService.QueryVideoById(videoId)
+		video := cc.videoService.QueryVideoById(req.VideoId)
 		t := time.Now()
 		comment := service.CommentParams{
-			UserId:     userId,
-			VideoId:    videoId,
+			UserId:     req.UserId,
+			VideoId:    req.VideoId,
 			Content:    content,
 			CreateDate: t,
 		}
@@ -66,8 +67,8 @@ func (cc *CommentController) CommentAction(c *gin.Context) {
 			})
 			return
 		}
-		userInfo, _ := cc.userService.QueryUserInfoByID(userId)
-		isFollow, _ := cc.relationService.IsFollowed(userId, video.AuthorID)
+		userInfo, _ := cc.userService.QueryUserInfoByID(req.UserId)
+		isFollow, _ := cc.relationService.IsFollowed(req.UserId, video.AuthorID)
 		userInfo.IsFollow = isFollow
 		c.JSON(http.StatusOK, CommentActionResponse{
 			Response: Response{StatusCode: code, StatusMsg: message},
@@ -80,9 +81,7 @@ func (cc *CommentController) CommentAction(c *gin.Context) {
 		})
 		return
 	} else {
-		cId := c.Query("comment_id")
-		commentId, _ := strconv.ParseInt(cId, 10, 64)
-		code, message := cc.commentService.DeleteComment(commentId)
+		code, message := cc.commentService.DeleteComment(req.CommentId)
 		c.JSON(http.StatusOK, Response{StatusCode: code, StatusMsg: message})
 		return
 	}
@@ -90,26 +89,14 @@ func (cc *CommentController) CommentAction(c *gin.Context) {
 
 // CommentList GET /douyin/comment/list/ 评论列表
 func (cc *CommentController) CommentList(c *gin.Context) {
-	id := c.Query("video_id")
-	videoId, _ := strconv.ParseInt(id, 10, 64)
-	video := cc.videoService.QueryVideoById(videoId)
-	comments := cc.commentService.QueryCommentsByVideoId(videoId)
-	// var commentList []CommentInfo
-	// for _, comment := range comments {
-	// 	user, err := cc.userService.QueryUserByID(comment.UserId)
-	// 	if err != nil {
-	// 		continue
-	// 	}
-	// 	userInfo, _ := cc.userService.QueryUserInfoByID(user.ID)
-	// 	isFollow, _ := cc.relationService.IsFollowed(user.ID, video.AuthorID)
-	// 	userInfo.IsFollow = isFollow
-	// 	commentList = append(commentList, CommentInfo{
-	// 		Id:         comment.Id,
-	// 		User:       userInfo,
-	// 		Content:    comment.Content,
-	// 		CreateDate: utils.TimeToStr(comment.CreateDate),
-	// 	})
-	// }
+	req, valid := validation.CommentListParseAndValidateParams(c)
+	if !valid {
+		c.JSON(http.StatusBadRequest, douyinPublishActionResponse{
+			Response: Response{-1, "Invalid Request."},
+		})
+		return
+	}
+	comments := cc.commentService.QueryCommentsByVideoId(req.Video.ID)
 	commentList := make([]CommentInfo, len(comments))
 	var wg sync.WaitGroup
 	wg.Add(len(comments))
@@ -121,7 +108,7 @@ func (cc *CommentController) CommentList(c *gin.Context) {
 				return
 			}
 			userInfo, _ := cc.userService.QueryUserInfoByID(user.ID)
-			isFollow, _ := cc.relationService.IsFollowed(user.ID, video.AuthorID)
+			isFollow, _ := cc.relationService.IsFollowed(user.ID, req.Video.AuthorID)
 			userInfo.IsFollow = isFollow
 			commentList[idx] = CommentInfo{
 				Id:         comment.Id,
@@ -137,3 +124,52 @@ func (cc *CommentController) CommentList(c *gin.Context) {
 		CommentList: commentList,
 	})
 }
+
+// // CommentAction POST /douyin/comment/action/ 评论操作
+// func (cc *CommentController) CommentAction(c *gin.Context) {
+// 	actionType := c.Query("action_type")
+// 	if actionType == "1" {
+// 		content := c.Query("comment_text")
+// 		content = utils.Filter.Replace(content, '#')
+// 		// 获取当前用户
+// 		userId := c.GetInt64("id")
+// 		// 获取当前视频
+// 		id := c.Query("video_id")
+// 		videoId, _ := strconv.ParseInt(id, 10, 64)
+// 		video := cc.videoService.QueryVideoById(videoId)
+// 		t := time.Now()
+// 		comment := service.CommentParams{
+// 			UserId:     userId,
+// 			VideoId:    videoId,
+// 			Content:    content,
+// 			CreateDate: t,
+// 		}
+// 		commentId, code, message := cc.commentService.PostComment(comment)
+// 		if code != 0 {
+// 			c.JSON(http.StatusOK, Response{
+// 				StatusCode: code,
+// 				StatusMsg:  message,
+// 			})
+// 			return
+// 		}
+// 		userInfo, _ := cc.userService.QueryUserInfoByID(userId)
+// 		isFollow, _ := cc.relationService.IsFollowed(userId, video.AuthorID)
+// 		userInfo.IsFollow = isFollow
+// 		c.JSON(http.StatusOK, CommentActionResponse{
+// 			Response: Response{StatusCode: code, StatusMsg: message},
+// 			CommentInfo: CommentInfo{
+// 				Id:         commentId,
+// 				User:       userInfo,
+// 				Content:    content,
+// 				CreateDate: utils.TimeToStr(t),
+// 			},
+// 		})
+// 		return
+// 	} else {
+// 		cId := c.Query("comment_id")
+// 		commentId, _ := strconv.ParseInt(cId, 10, 64)
+// 		code, message := cc.commentService.DeleteComment(commentId)
+// 		c.JSON(http.StatusOK, Response{StatusCode: code, StatusMsg: message})
+// 		return
+// 	}
+// }
---
+++ utils/validation/commentValidator.go
@@ -0,0 +1,78 @@
+package validation
+
+import (
+	"reflect"
+	"strconv"
+
+	"github.com/Shanwu404/TikTokLite/service"
+	"github.com/gin-gonic/gin"
+)
+
+type CommentActionRequest struct {
+	UserId     int64
+	VideoId    int64
+	ActionType string
+	Content    string
+	CommentId  int64
+}
+
+type CommentListRequest struct {
+	Video service.VideoParams
+}
+
+func CommentActionParseAndValidateParams(c *gin.Context) (CommentActionRequest, bool) {
+	req := CommentActionRequest{}
+	usi := service.NewUserService()
+	vsi := service.NewVideoService()
+
+	// 判断 userId 是否存在
+	userId := c.GetInt64("id")
+	flag := usi.IsUserIdExist(userId)
+	if !flag {
+		return req, false
+	}
+	req.UserId = userId
+
+	// 判断 videoId 是否存在
+	id := c.Query("video_id")
+	videoId, _ := strconv.ParseInt(id, 10, 64)
+	video := vsi.QueryVideoById(videoId)
+	if reflect.DeepEqual(video, service.VideoParams{}) {
+		return req, false
+	}
+	req.VideoId = videoId
+
+	// 判读操作类别
+	actionType := c.Query("action_type")
+	if actionType != "1" && actionType != "2" {
+		return req, false
+	}
+	req.ActionType = actionType
+	if actionType == "1" {
+		content := c.Query("comment_text")
+		if len(content) > 500 {
+			return req, false
+		}
+		req.Content = content
+		return req, true
+	} else {
+		cId := c.Query("comment_id")
+		commentId, _ := strconv.ParseInt(cId, 10, 64)
+		req.CommentId = commentId
+		return req, true
+	}
+}
+
+func CommentListParseAndValidateParams(c *gin.Context) (CommentListRequest, bool) {
+	req := CommentListRequest{}
+	vsi := service.NewVideoService()
+
+	id := c.Query("video_id")
+	videoId, _ := strconv.ParseInt(id, 10, 64)
+	video := vsi.QueryVideoById(videoId)
+	if reflect.DeepEqual(video, service.VideoParams{}) {
+		return req, false
+	}
+	req.Video = video
+	return req, true
+}
